1f14dc0f01b20cf3ae9fda87e53f4b01104c9ec7
XY <fxy0111@gmail.com>
2013-01-31 13:26:39 -0500

    merge code after home page done

---
+++ web/public/js/navbar.js
@@ -0,0 +1,34 @@
+$(document).ready(
+  function(){
+    $('#header .navbar').waypoint('sticky');
+    $('#footer')
+      .waypoint(function(direction) {
+        $('#header .navbar').toggleClass('bottom');
+      }, {
+        offset: 120
+      });
+
+    $('.receipt[data-toggle="sticky"]')
+      .waypoint(function(direction) {
+        $(this).toggleClass('stuck', direction === 'down');
+      }, {
+        offset: 90,
+      })
+      .waypoint(function(direction) {
+        $(this).toggleClass('bottom', direction === 'down');
+      }, {
+        offset: function() {
+          return -($('.left-column').outerHeight()-90-$(this).height());
+        }
+      });
+
+    $('[data-toggle="collapse"]').click(
+      function(event) {
+        event.preventDefault();
+        var $target = $($(this).attr('href'));
+        $(this).toggleClass('open');
+        $target.toggleClass('in');
+      }
+    );
+  }
+);
