7c747c9d3bdc1451ea5fc741880c047397e21acf
Kyle Crouse <kyle.crouse@gmail.com>
2013-01-31 09:45:15 -0500

    adding nav back to navbar and perfecting the waypoints for the receipt docking

---
+++ web/public/js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(
+  function(){
+    $('#header .navbar').waypoint('sticky');
+    //$('.receipt').waypoint('sticky',{'offset':90});
+
+    $('.receipt')
+      .waypoint(function(direction) {
+        $(this).toggleClass('stuck', direction === 'down');
+      }, {
+        offset: 90,
+      })
+      .waypoint(function(direction) {
+        $(this).toggleClass('bottom', direction === 'down');
+      }, {
+        offset: function() {
+          return -($('.left-column').outerHeight()-90-$(this).height());
+        }
+      });
+  }
+);
